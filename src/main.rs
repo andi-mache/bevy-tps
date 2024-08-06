dfe64fe33e7a5b387d4e84e84da5d1bc0165594b
andi.mache <amache75@gmail.com>
2024-08-06 14:33:40 +0300

    bye

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,79 @@
-use bevy::prelude::*;
 
+use bevy::{ prelude::*, DefaultPlugins};
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
+        .add_systems(
+            Startup, 
+            ( spawn_player,spawn_camera,spawn_floor, spawn_light)
+        )
+        .add_systems(Update, example_ui)
+        .run();
 
+}
+
+//lets add a camera
+fn spawn_camera(mut commands: Commands) {
+    let camera = Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    };
+    commands.spawn(camera);
 }
 
 // lets define a floor 
 fn spawn_floor(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandartMaterial>>
+    mut materials: ResMut<Assets<StandardMaterial>>
 ) {
+    let floor = PbrBundle {
+        mesh: meshes.add(Mesh::from(Plane3d::default().mesh().size(15.0, 15.0))),
+        material: materials.add(Color::srgb(0.7, 0.6, 0.5)),
+        ..default()
+    };
+
+    commands.spawn(floor);
+}
+
+//spawn a player
+fn spawn_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let player = PbrBundle {
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::srgb_u8(124, 144, 255)),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    };
+
+    commands.spawn(player);
+}
+
+//spawn some light
+fn spawn_light(mut commands: Commands) {
+    let light = PointLightBundle {
+        point_light: PointLight {
+            intensity: 2000.0 * 1000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 5.0, 0.0),
+        ..default()
+    };
+
+    commands.spawn(light);
     
+}
+
+fn example_ui(mut contexts: EguiContexts){
+    egui::Window::new("Bonjour").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    } );
 }
